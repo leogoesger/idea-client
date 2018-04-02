365c96f856a42e5e1a416250f9adad14b50c3529
jaseat <jaseat@users.noreply.github.com>
2018-04-02 16:38:02 -0700

    NEL-5: Added links to navbar

--- src/components/navbar/Layout.js
+++ src/components/navbar/Layout.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Drawer from 'material-ui/Drawer';
 import List, { ListItem, ListItemText } from 'material-ui/List';
 import Divider from 'material-ui/Divider';
+import {navigateTo} from '../../utils/helpers'
 
 export default class Layout extends React.Component {
   render() {
@@ -10,25 +11,28 @@ export default class Layout extends React.Component {
       <Drawer variant="permanent" anchor="left">
         <Divider />
         <List>
-          <ListItem button>
-            <ListItemText primary="Home" />
+          <ListItem button onClick={navigateTo.bind(this, "/", "")}>
+            <ListItemText primary="Home"  />
           </ListItem>
-          <ListItem button>
+          <ListItem button onClick={navigateTo.bind(this, "/team", "")}>
+            <ListItemText primary="Team"  />
+          </ListItem>
+          <ListItem button onClick={navigateTo.bind(this, "/service", "")}>
             <ListItemText primary="Services" />
           </ListItem>
-          <ListItem button>
+          <ListItem button onClick={navigateTo.bind(this, "/contract", "")}>
             <ListItemText primary="Contracts" />
           </ListItem>
-          <ListItem button>
+          <ListItem button onClick={navigateTo.bind(this, "/portfolio", "")}>
             <ListItemText primary="Portfolio" />
           </ListItem>
-          <ListItem button>
+          <ListItem button onClick={navigateTo.bind(this, "/contact", "")}>
             <ListItemText primary="Contact Us" />
           </ListItem>
         </List>
         <Divider />
         <List>
-          <ListItem button>
+          <ListItem button onClick={navigateTo.bind(this, "/login", "")}>
             <ListItemText primary="Login" />
           </ListItem>
         </List>
