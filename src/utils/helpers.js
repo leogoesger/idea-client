284590752f4e599e953baff639ddf850bb3fd51d
Leo Q <leoq91@gmail.com>
2018-04-03 16:33:33 -0700

    NEL-18: remove un-used

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,4 @@
-import React from 'react';
-
 import {history} from '../store/configureStore';
-import {Link} from 'react-router-dom';
 
 export function navigateTo(pathname, query) {
   history.push({pathname, query});
